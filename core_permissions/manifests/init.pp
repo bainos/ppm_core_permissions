1de1e44ceaef84311f065034ae6b3fcaa3f70f25
Jacopo Binosi <b4inoz@gmail.com>
2014-02-09 13:29:43 +0100

    Initial commit.
    Menage permission for critical system files.

---
+++ core_permissions/manifests/init.pp
@@ -0,0 +1,50 @@
+# == Class: core_permission
+#
+# This class menages permissions for critical system files.
+#
+# Original example:
+#   http://docs.puppetlabs.com/pe/latest/quick_writing.html
+#
+# Rewritten using puppen-lint.
+#
+# === Authors
+#
+# Jacopo Binosi <b4inoz@gmail.com>
+#
+class core_permissions {
+  if $::osfamily != 'windows' {
+
+    $rootgroup = $::operatingsystem ? {
+      'Solaris' => 'wheel',
+      default   => 'root',
+    }
+
+    $fstab = $::operatingsystem ? {
+      'Solaris' => '/etc/vfstab',
+      default   => '/etc/fstab',
+    }
+
+    file {'fstab':
+      ensure => present,
+      path   => $fstab,
+      mode   => '0644',
+      owner  => 'root',
+      group  => $rootgroup,
+    }
+
+    file {'/etc/passwd':
+      ensure => present,
+      mode   => '0644',
+      owner  => 'root',
+      group  => $rootgroup,
+    }
+
+    file {'/etc/crontab':
+      ensure => present,
+      mode   => '0644',
+      owner  => 'root',
+      group  => $rootgroup,
+    }
+
+  }
+}
